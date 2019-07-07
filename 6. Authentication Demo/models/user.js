aa9846cf06aee2425b0545a0ea2c8c470b6d03bb
sauravdas97 <49933644+sauravdas97@users.noreply.github.com>
2019-07-07 11:32:13 +0530

    Delete user.js

--- 6. Authentication Demo/models/user.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongoose")
-var userSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-userSchema.plugin(passportLocalMongoose); // here the package "passport-local-mongoose" will add all the packages top the user schema that we created for authentication
-module.exports = mongoose.model("User",userSchema);
